981fd14bc686cea23c2f3bd394c2fbe5fcae95c2
Your Name <aktifman8@gmail.com>
2025-07-22 05:38:36 +0600

    fix code

--- src/main.py
+++ src/main.py
@@ -1,26 +1,39 @@
 import flet as ft
+import json
+import aiofiles
 
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+async def main(page: ft.Page):
+    async with aiofiles.open('src/config/user_settings.json', 'r') as file:
+        data = json.loads(await file.read())
+    language_system = data['system_language']
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    if language_system == 'Russian':
+        from system.ru.navigation.FletRouter import Router
+        from system.ru.navigation.bar import bottomappbar, appbar
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+        page.title = f'Kulpu: менеджер паролей'
+        page.theme_mode = "dark"
+        page.scroll = 'HIDDEN'
+        page.padding = 10
+
+        page.bgcolor = None
+        page.window.width = 390
+        page.window.height = 700
+        page.adaptive = True
+
+        page.appbar = await appbar(page)
+        page.bottom_appbar = await bottomappbar(page)
+
+        myRouter = Router()
+        await myRouter.init(page)
+        page.on_route_change = myRouter.route_change
+        page.add(
+            myRouter.body
         )
-    )
+        page.go('/')
 
-
-ft.app(main)
+ft.app(
+    target=main,
+    assets_dir="assets",
+)
